fdb936e8404236559a8e971a5324f1ff9cb9e5dc
Ehfaz Rezwan <ehfaz.rezwan@gmail.com>
2021-11-16 19:58:25 +0600

    Updated codebase to generate jokes on button click

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import useRandomJoke from "./useRandomJoke";
 
 function App() {
-  const joke = useRandomJoke("Ehfaz", "Rezwan");
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("Ehfaz");
+  const [lastName, setLastName] = useState("Rezwan");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
 
   return (
     <div className="App">
@@ -11,7 +24,14 @@ function App() {
 
         <h2>{joke}</h2>
 
-        <button>Generate joke</button>
+        <form>
+          <input placeholder="First name" ref={firstNameRef} />
+          <input placeholder="Last name" ref={lastNameRef} />
+
+          <button onClick={generateJoke} type="submit">
+            Generate joke
+          </button>
+        </form>
       </center>
     </div>
   );
